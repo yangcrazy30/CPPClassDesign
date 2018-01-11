fd92bd72ab325dab6144fc09c779f146d29386d6
yangcrazy30 <32866801+yangcrazy30@users.noreply.github.com>
2018-01-11 15:21:03 +0800

    Add files via upload

---
+++ Food.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    class Food
+    {
+        private string foodname;
+        public string FoodName
+        {
+            get
+            {
+                return foodname;
+            }
+            set
+            {
+                foodname = value;
+            }
+        }
+        private float price;
+        public float Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+        private int ordercount;
+        public int OrderCount
+        {
+            get
+            {
+                return ordercount;
+            }
+            set
+            {
+                ordercount = value;
+            }
+        }
+        private string foodID;
+        public string FoodID
+        {
+            get { return foodID; }
+            set
+            {
+                foodID = value;
+            }
+        }
+    }
+}
---
+++ MenuCreate.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Restaurant
+{
+    partial class MenuCreate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.菜名 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.价钱 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.编号 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.评分 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.InsertButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.编号,
+            this.菜名,
+            this.价钱,
+            this.评分});
+            this.listView1.Location = new System.Drawing.Point(12, 26);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(280, 199);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // 菜名
+            // 
+            this.菜名.DisplayIndex = 0;
+            this.菜名.Text = "菜名";
+            // 
+            // 价钱
+            // 
+            this.价钱.DisplayIndex = 1;
+            this.价钱.Text = "价钱";
+            // 
+            // 编号
+            // 
+            this.编号.DisplayIndex = 2;
+            this.编号.Text = "编号";
+            // 
+            // 评分
+            // 
+            this.评分.Text = "评分";
+            // 
+            // InsertButton
+            // 
+            this.InsertButton.Location = new System.Drawing.Point(321, 26);
+            this.InsertButton.Name = "InsertButton";
+            this.InsertButton.Size = new System.Drawing.Size(75, 23);
+            this.InsertButton.TabIndex = 1;
+            this.InsertButton.Text = "添加";
+            this.InsertButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(321, 77);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 2;
+            this.DeleteButton.Text = "删除";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 266);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.InsertButton);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader 编号;
+        private System.Windows.Forms.ColumnHeader 菜名;
+        private System.Windows.Forms.ColumnHeader 价钱;
+        private System.Windows.Forms.ColumnHeader 评分;
+        private System.Windows.Forms.Button InsertButton;
+        private System.Windows.Forms.Button DeleteButton;
+    }
+}
